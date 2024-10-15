0b08759c4ac51cc3456a44ae9453e69d10a31c4c
OctagonalDefence <eromero110@boscdelacoma.cat>
2024-10-15 19:10:48 +0200

    user registry's style

--- Registres_curriculars_CRPGA/src/app/app.component.ts
+++ Registres_curriculars_CRPGA/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RegistresUComponent } from "./components/registres-u/registres-u.component";
+import { SdaComponent } from './components/sda/sda.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RegistresUComponent, SdaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- Registres_curriculars_CRPGA/src/app/app.routes.ts
+++ Registres_curriculars_CRPGA/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { SdaComponent } from './components/sda/sda.component';
+import { RegistresUComponent } from './components/registres-u/registres-u.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'sda', component: SdaComponent},
+    {path: 'registres-u', component: RegistresUComponent},
+
+];
