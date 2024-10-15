d010483c4d44c670743b7adc9959a13dc82967e7
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2024-10-15 18:34:41 +0200

    Test should create input Description

--- Registres_curriculars_CRPGA/src/app/test/components/sda.component.spec.ts
+++ Registres_curriculars_CRPGA/src/app/test/components/sda.component.spec.ts
@@ -9,14 +9,13 @@ describe('SdaComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SdaComponent]
-    })
-    .compileComponents();
+      imports: [SdaComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(SdaComponent);
     component = fixture.componentInstance;
     compiled = fixture.nativeElement as HTMLElement;
-   
+
     fixture.detectChanges();
   });
 
@@ -24,17 +23,15 @@ describe('SdaComponent', () => {
     expect(component).toBeTruthy();
   });
   it('should create the Header', () => {
-    
     const div = compiled.querySelector('div');
     const p = div?.querySelector('p');
     const anchor = div?.querySelector('a');
     expect(div).toBeTruthy();
-   
+
     expect(anchor).toBeTruthy();
     expect(anchor?.innerHTML).toContain('⇦');
     expect(p).toBeTruthy();
     expect(p?.textContent).toContain('Usuari');
-    
   });
   it('should create input Title', () => {
     const div = compiled.querySelectorAll('div')[1];
@@ -45,8 +42,15 @@ describe('SdaComponent', () => {
     expect(p.textContent).toContain('Títol');
     expect(input).toBeTruthy();
     expect(input?.getAttribute('value')).toContain('Títol');
-
-   
-});
-  
+  });
+  it('should create input Description', () => {
+    const div = compiled.querySelectorAll('div')[2];
+    const p = div.querySelectorAll('p')[0];
+    const input = div.querySelector('input');
+    expect(div).toBeTruthy();
+    expect(p).toBeTruthy();
+    expect(p.textContent).toContain('Descripció');
+    expect(input).toBeTruthy();
+    expect(input?.getAttribute('value')).toContain('Descripció');
+  });
 });
