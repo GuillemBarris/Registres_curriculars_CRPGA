d04f3d585c9224937408bd2adf8670731086478f
OctagonalDefence <eromero110@boscdelacoma.cat>
2024-10-15 19:22:27 +0200

    h

--- Registres_curriculars_CRPGA/src/app/components/registres-u/registres-u.component.ts
+++ Registres_curriculars_CRPGA/src/app/components/registres-u/registres-u.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-registres-u',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './registres-u.component.html',
   styleUrl: './registres-u.component.css'
 })
