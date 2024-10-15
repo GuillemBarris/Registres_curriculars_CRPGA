f49ebe5f16364b6ac0e3c969f1b77d47fcf3c875
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2024-10-15 20:05:52 +0200

    Solucionar problema ruterModule RouterLink

--- Registres_curriculars_CRPGA/src/app/components/registres-u/registres-u.component.ts
+++ Registres_curriculars_CRPGA/src/app/components/registres-u/registres-u.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-registres-u',
   standalone: true,
-  imports: [RouterModule],
+  imports: [],
   templateUrl: './registres-u.component.html',
   styleUrl: './registres-u.component.css'
 })
