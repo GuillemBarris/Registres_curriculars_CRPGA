aaf3b800f033beba3db14ca39cad75d5c6351d44
OctagonalDefence <eromero110@boscdelacoma.cat>
2024-10-18 16:45:02 +0200

    summary UI

--- Registres_curriculars_CRPGA/src/app/app.routes.ts
+++ Registres_curriculars_CRPGA/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { SdaComponent } from './components/sda/sda.component';
 import { RegistresUComponent } from './components/registres-u/registres-u.component';
 import { LoginComponent } from './components/login/login.component';
+import { SummaryComponent } from './components/summary/summary.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'login', pathMatch: 'full'},
     {path: 'sda', component: SdaComponent},
     {path: 'registres-u', component: RegistresUComponent},
-    {path: 'login', component: LoginComponent}
+    {path: 'login', component: LoginComponent},
+    {path:'summary', component: SummaryComponent}
 
 ];
