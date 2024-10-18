eff2068572794779bcd92e36ccb9a0fdc9844563
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2024-10-18 15:59:51 +0200

    Solve Hydration problem

--- Registres_curriculars_CRPGA/src/app/app.routes.ts
+++ Registres_curriculars_CRPGA/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { SdaComponent } from './components/sda/sda.component';
 import { RegistresUComponent } from './components/registres-u/registres-u.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
     {path: 'sda', component: SdaComponent},
     {path: 'registres-u', component: RegistresUComponent},
+    {path: 'login', component: LoginComponent}
 
 ];
