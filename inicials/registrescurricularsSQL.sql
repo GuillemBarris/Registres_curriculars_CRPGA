1f2658f5dc7bac4b68878474011fb4c4f4a21e13
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2024-10-18 19:14:08 +0200

    Refactor registrescurricularsSQL.sql

--- inicials/registrescurricularsSQL.sql
+++ inicials/registrescurricularsSQL.sql
@@ -3,98 +3,95 @@ CREATE DATABASE RegistresCurriculars;
 USE RegistresCurriculars;
 
 CREATE TABLE Centre (
-    Nom VARCHAR(255) PRIMARY KEY,
-    Logo VARCHAR(255)
+    Nom NVARCHAR(255) PRIMARY KEY,
+    Logo NVARCHAR(255)
 );
 
 CREATE TABLE Usuari (
-    Nom VARCHAR(255) NOT NULL,
-    Correu VARCHAR(255) PRIMARY KEY,
-    Centre_Nom VARCHAR(255),
-    Contrasenya VARCHAR(255),
-    Tipus VARCHAR(255),
+    Nom NVARCHAR(255) NOT NULL,
+    Correu NVARCHAR(255) PRIMARY KEY,
+    Centre_Nom NVARCHAR(255),
+    Contrasenya NVARCHAR(255),
+    Tipus NVARCHAR(255),
     FOREIGN KEY (Centre_Nom) REFERENCES Centre(Nom)
 );
 
 CREATE TABLE Historial (
-    id INT AUTO_INCREMENT PRIMARY KEY
+    ID UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY
 );
 
 CREATE TABLE Curs (
-    Nom VARCHAR(255) NOT NULL,
-    Grup VARCHAR(255),
-    Any INT NOT NULL,
-    PRIMARY KEY (Nom, Any, Grup),    
-    Usuari_correu VARCHAR(255),
-    Historial_id INT,
+    Nom NVARCHAR(255) NOT NULL,
+    Grup NVARCHAR(255),
+    "Any" INT NOT NULL,
+    Usuari_correu NVARCHAR(255),
+	PRIMARY KEY (Nom, "Any", Grup),   
     FOREIGN KEY (Usuari_correu) REFERENCES Usuari(Correu),
-    FOREIGN KEY (Historial_id) REFERENCES Historial(id)
 );
 
 CREATE TABLE Projecte (
-    Titol VARCHAR(255) PRIMARY KEY,
-    Link VARCHAR(255),
+    Titol NVARCHAR(255),
+    Link NVARCHAR(255),
     Data_Inici DATE,
     Data_Fi DATE,    
     Descripcio TEXT,    
-    Curs_Nom VARCHAR(255),
-    Curs_Any INT,
-    Curs_Grup VARCHAR(255),
-    FOREIGN KEY (Curs_Nom) REFERENCES Curs(Nom),
-    FOREIGN KEY (Curs_Any) REFERENCES Curs(Any),
-    FOREIGN KEY (Curs_Grup) REFERENCES Curs(Grup)
+    Curs_Nom NVARCHAR(255) Not Null,
+	Curs_Grup NVARCHAR(255),
+    Curs_Any INT Not Null,
+    PRIMARY Key (Titol, Data_Inici),
+    FOREIGN KEY (Curs_Nom, Curs_Any, Curs_Grup) REFERENCES Curs(Nom,"Any",Grup),
+    
 );
 
 CREATE TABLE Competencia (
-    Nom VARCHAR(255) PRIMARY KEY,
-    Tipus VARCHAR(255),
-    Projecte_Titol VARCHAR(255),
-    FOREIGN KEY (Projecte_Titol) REFERENCES Projecte(Titol)
+    Nom NVARCHAR(255) PRIMARY KEY,
+    Tipus NVARCHAR(255),
+    Projecte_Titol NVARCHAR(255),
+	Projecte_Data_Inici DATE,
+    FOREIGN KEY (Projecte_Titol, Projecte_Data_Inici) REFERENCES Projecte(Titol, Data_Inici),
     
 );
 
 CREATE TABLE Assignatura (
-    Nom VARCHAR(255) PRIMARY KEY,
-    Competencia_Nom VARCHAR(255),
+    Nom NVARCHAR(255) PRIMARY KEY,
+    Competencia_Nom NVARCHAR(255),
     FOREIGN KEY (Competencia_Nom) REFERENCES Competencia(Nom)
 );
 
 CREATE TABLE Criteri (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    ID UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY,
     Descripcio TEXT NOT NULL,
-    Assignatura_Nom VARCHAR(255),
+    Assignatura_Nom NVARCHAR(255),
     FOREIGN KEY (Assignatura_Nom) REFERENCES Assignatura(Nom)
 );
 
 CREATE TABLE Avaluacio (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    ID UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY,
     Descripcio TEXT NOT NULL,
-    Criteri_id INT,
+    Criteri_id UNIQUEIDENTIFIER,
     FOREIGN KEY (Criteri_id) REFERENCES Criteri(id)
 );
 
 CREATE TABLE Categoria (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    ID UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY,
     Descripcio TEXT NOT NULL,
-    Assignatura_Nom VARCHAR(255),
+    Assignatura_Nom NVARCHAR(255),
     FOREIGN KEY (Assignatura_Nom) REFERENCES Assignatura(Nom)
 );
 
 CREATE TABLE Sabers (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    ID UNIQUEIDENTIFIER DEFAULT NEWID() PRIMARY KEY,
     Descripcio TEXT NOT NULL,
-    Categoria_id INT,
+    Categoria_id UNIQUEIDENTIFIER,
     FOREIGN KEY (Categoria_id) REFERENCES Categoria(id)
 );
 
 CREATE TABLE Curs_Historial (
-    Curs_Nom VARCHAR(255),
+    Curs_Nom NVARCHAR(255),
     Curs_Any INT,
-    Curs_Grup VARCHAR(255),
-    Historial_id INT,
+    Curs_Grup NVARCHAR(255),
+    Historial_id UNIQUEIDENTIFIER,
     PRIMARY KEY (Curs_Nom, Curs_Any, Curs_Grup, Historial_id),
-    FOREIGN KEY (Curs_Nom) REFERENCES Curs(Nom),
-    FOREIGN KEY (Curs_Any) REFERENCES Curs(Any),
-    FOREIGN KEY (Curs_Grup) REFERENCES Curs(Grup),
+    FOREIGN KEY (Curs_Nom,Curs_Any, Curs_Grup) REFERENCES Curs(Nom, "Any", Grup),
     FOREIGN KEY (Historial_id) REFERENCES Historial(id)
 );
