7c1bbc69768ef6739bbf288b4d7759c4681c7d01
OctagonalDefence <eromero110@boscdelacoma.cat>
2024-11-19 17:12:37 +0100

    tests fix

--- Registres_curriculars_CRPGA/src/app/test/components/app.component.spec.ts
+++ Registres_curriculars_CRPGA/src/app/test/components/app.component.spec.ts
@@ -20,10 +20,5 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('Registres_curriculars_CRPGA');
   });
 
-   /*it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Registres_curriculars_CRPGA');
-  });*/
+  
 });
