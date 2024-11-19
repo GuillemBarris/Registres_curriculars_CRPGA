fa7550e3b6ebab2b55901ad9633192b956a4f644
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2024-11-19 16:04:53 +0100

    Test should add new Name

--- Registres_curriculars_CRPGA/src/app/test/components/user-school.component.spec.ts
+++ Registres_curriculars_CRPGA/src/app/test/components/user-school.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
 import { UserSchoolComponent } from '../../components/user-school/user-school.component';
 
 describe('UserSchoolComponent', () => {
@@ -20,4 +20,14 @@ describe('UserSchoolComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should add new Name', async () => {
+    const classInput = fixture.debugElement.query(By.css('input[placeholder="Name"]'));
+    classInput.nativeElement.value = 'Eric';
+    classInput.nativeElement.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+    await fixture.whenStable();
+    expect(classInput.nativeElement.value).toBe('Eric');
+    
+  });
+  
 });
