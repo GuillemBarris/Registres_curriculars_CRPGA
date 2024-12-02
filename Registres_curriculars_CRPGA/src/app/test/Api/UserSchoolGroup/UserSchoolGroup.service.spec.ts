7d668a020b60d8bc826b470d9bb0fbeaa73954ad
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2024-12-02 18:44:45 +0100

    Test should fetch groups list

---
+++ Registres_curriculars_CRPGA/src/app/test/Api/UserSchoolGroup/UserSchoolGroup.service.spec.ts
@@ -0,0 +1,48 @@
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { UserSchoolGroupService } from './UserSchoolGroup.service.js';
+
+describe('UserSchoolGroupService', () => {
+    let service: UserSchoolGroupService;
+    let httpMock: HttpTestingController;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            providers: [UserSchoolGroupService]
+        });
+        service = TestBed.inject(UserSchoolGroupService);
+        httpMock = TestBed.inject(HttpTestingController);
+    });
+
+    afterEach(() => {
+        httpMock.verify();
+    });
+
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+    it('should fetch groups list', () => {
+        const mockGroups = {
+            "groups": [
+                {
+                    "name": "A"
+                },
+                {
+                    "name": "B"
+                },
+                {
+                    "name": "C"
+                }
+            ]
+        };
+       service.getGroups().subscribe(groups => {
+            expect(groups).toEqual(mockGroups);
+        }
+       );
+       const req = httpMock.expectOne(`${service.baseUrl}/groups`);
+       expect(req.request.method).toBe('GET');
+         req.flush(mockGroups);
+    });
+
+});
