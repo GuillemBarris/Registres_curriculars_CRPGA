c84e9554095840d71de4a20d55a32920168b8f6c
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2024-12-02 17:02:13 +0100

    GET:  getGrade

--- API/controllers/UserSchoolGroupController.js
+++ API/controllers/UserSchoolGroupController.js
@@ -35,3 +35,14 @@ export const getGroup = async (req, res) => {
         res.status(500).json({ message: 'Error getting groups', error: err.message });
     }
 };
+
+export const getGrade = async (req, res) => {
+    try {
+        let pool = await sql.connect(config);
+        let result = await pool.request()
+            .query('USE Registres_Curriculars; SELECT DISTINCT Grade FROM User_School_Group');
+        res.status(200).json(result.recordset);
+    } catch (err) {
+        res.status(500).json({ message: 'Error getting grades', error: err.message });
+    }
+}
--- API/routes/UserSchoolGroupRoutes.js
+++ API/routes/UserSchoolGroupRoutes.js
@@ -1,10 +1,11 @@
 
 import { Router } from "express";
-import { createUserSchoolGroup, getGroup } from '../controllers/UserSchoolGroupController.js';
+import { createUserSchoolGroup, getGroup, getGrade } from '../controllers/UserSchoolGroupController.js';
 
 const UserSchoolGroup = Router();
 
 UserSchoolGroup.post("/createUserSchoolGroup/", createUserSchoolGroup);
 UserSchoolGroup.get("/getGroup/", getGroup);
+UserSchoolGroup.get("/getGrade/", getGrade);
 
 export default UserSchoolGroup;
