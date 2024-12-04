08de410d9addadbd3e45a7d97c5516c3117c371f
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2024-12-04 17:51:55 +0100

    implement Test should fetch groups list

---
+++ Registres_curriculars_CRPGA/src/app/services/UserShoolGroupService.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from 'rxjs';
+@Injectable({
+    providedIn:'root'
+})
+export class UserSchoolGroupService {
+    private dbUrl = 'http://172.21.46.184:3000/api/v1/userSchoolGroup';
+
+    constructor(private http: HttpClient) {}
+
+    getGroups(): Observable<any> {
+        return this.http.get<any>(`${this.dbUrl}/getGroup/`);
+    }
+}
