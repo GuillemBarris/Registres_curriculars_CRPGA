3ec9ef28ae653b2979c9a3624fd8d441660ae65b
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2024-12-09 18:30:01 +0100

    install and put cors in the API

--- API/server.js
+++ API/server.js
@@ -1,14 +1,18 @@
 import express from 'express';
+import cors from 'cors';
 import UserRoutes from './routes/UsersRoutes.js';
 import UserSchoolGroup from './routes/UserSchoolGroupRoutes.js';
 
-const app = express()
+const app = express();
 
-const PORT = 3000
-const IP = '192.168.222.92'
+const PORT = 3000;
+const IP = '172.21.46.184';
+
+app.use(cors()); 
 app.use(express.json());
 app.use("/api/v1/users", UserRoutes);
 app.use("/api/v1/userSchoolGroup", UserSchoolGroup);
+
 app.listen(PORT, IP, () => {
-    console.log(`Server is running on http://${IP}:${PORT}`)
+    console.log(`Server is running on http://${IP}:${PORT}`);
 });
