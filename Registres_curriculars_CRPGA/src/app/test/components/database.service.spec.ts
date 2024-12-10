51d222ccb69ec5932f3db89dbc7896fd5e3a0b6b
OctagonalDefence <eromero110@boscdelacoma.cat>
2024-12-10 15:18:30 +0100

    updated service tests

--- Registres_curriculars_CRPGA/src/app/test/components/database.service.spec.ts
+++ Registres_curriculars_CRPGA/src/app/test/components/database.service.spec.ts
@@ -26,32 +26,33 @@ describe('DatabaseService', () => {
   it('should return true if user is registered', () => {
     const dummyResponse = [{ email: 'test@example.com' }];
     service.isUserRegistered('test@example.com').subscribe((isRegistered) => {
-      expect(isRegistered).toBeTrue();
+        expect(isRegistered).toBeTrue();
     });
 
-    const req = httpMock.expectOne(`${service['dbUrl']}/getAllUsers?email=test@example.com`);
+    const req = httpMock.expectOne(`${service['dbUrl']}/getUserbyEmail/?email=test@example.com`);
     expect(req.request.method).toBe('GET');
     req.flush(dummyResponse);
-  });
+});
 
-  it('should return false if user is not registered', () => {
+it('should return false if user is not registered', () => {
     const dummyResponse: any[] = [];
     service.isUserRegistered('notregistered@example.com').subscribe((isRegistered) => {
-      expect(isRegistered).toBeFalse();
+        expect(isRegistered).toBeFalse();
     });
 
-    const req = httpMock.expectOne(`${service['dbUrl']}/getAllUsers?email=notregistered@example.com`);
+    const req = httpMock.expectOne(`${service['dbUrl']}/getUserbyEmail/?email=notregistered@example.com`);
     expect(req.request.method).toBe('GET');
     req.flush(dummyResponse);
-  });
+});
 
-  it('should handle error and return false', () => {
+it('should handle error and return false', () => {
     service.isUserRegistered('error@example.com').subscribe((isRegistered) => {
-      expect(isRegistered).toBeFalse();
+        expect(isRegistered).toBeFalse();
     });
 
-    const req = httpMock.expectOne(`${service['dbUrl']}/getAllUsers?email=error@example.com`);
+    const req = httpMock.expectOne(`${service['dbUrl']}/getUserbyEmail/?email=error@example.com`);
     expect(req.request.method).toBe('GET');
     req.error(new ErrorEvent('Network error'));
-  });
+});
+
 });
