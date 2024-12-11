6e1ac76547e6b6f98d4fbeb38469da288a04be90
OctagonalDefence <eromero110@boscdelacoma.cat>
2024-12-11 18:05:23 +0100

    admin-registres-u UI + Styling

--- Registres_curriculars_CRPGA/src/app/components/admin-registres-u/admin-registres-u.component.ts
+++ Registres_curriculars_CRPGA/src/app/components/admin-registres-u/admin-registres-u.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-admin-registres-u',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './admin-registres-u.component.html',
   styleUrl: './admin-registres-u.component.css'
 })
 export class AdminRegistresUComponent {
+
+  
 
 }
