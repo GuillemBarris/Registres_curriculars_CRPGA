eab84b25cfa75d836e39468c8b8c1820569c25b8
OctagonalDefence <eromero110@boscdelacoma.cat>
2024-12-11 20:19:37 +0100

    tests success

--- Registres_curriculars_CRPGA/src/app/components/admin-registres-u/admin-registres-u.component.ts
+++ Registres_curriculars_CRPGA/src/app/components/admin-registres-u/admin-registres-u.component.ts
@@ -10,6 +10,18 @@ import { CommonModule } from '@angular/common';
 })
 export class AdminRegistresUComponent {
 
+  dropdownVisibility: { [key: string]: boolean } = {};
+
+  toggleDropdown(id: string) {
+    console.log('Button clicked for dropdown:', id);
+    this.dropdownVisibility[id] = !this.dropdownVisibility[id];
+    console.log('Dropdown visibility for', id, ':', this.dropdownVisibility[id]);
+  }
+
+  isDropdownVisible(id: string): boolean {
+    return this.dropdownVisibility[id] || false;
+  }
+
   
 
 }
