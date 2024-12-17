18c2b5208f826c1cdb9d2ef71bc70303af056bb3
OctagonalDefence <eromero110@boscdelacoma.cat>
2024-12-17 19:11:05 +0100

    getCoursesFromTeacher method in Backend API

--- API/controllers/UserSchoolGroupController.js
+++ API/controllers/UserSchoolGroupController.js
@@ -34,3 +34,20 @@ export const getSubject = async (req, res) => {
         res.status(500).json({ message: 'Error getting subjects', error: err.message });
     }
 }
+
+export const getCoursesFromTeacher = async (req, res) => { 
+    try {
+        const { teacher } = req.query; 
+        if (!teacher) {
+            return res.status(400).json({ message: 'Teacher parameter is required' });
+        }
+        let pool = await sql.connect(config);
+        let result = await pool.request()
+            .input('teacher', sql.VarChar, teacher) 
+            .query('USE Registres_Curriculars; SELECT id_course FROM User_School_Group WHERE teacher = @teacher');
+        res.status(200).json(result.recordset);
+    } catch (err) {
+        res.status(500).json({ message: 'Error getting courses', error: err.message });
+    }
+    
+}
