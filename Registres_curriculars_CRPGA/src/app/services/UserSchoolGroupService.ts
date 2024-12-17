58e58c72f21f9ff8049c765ffe817288e824faca
GuillemBarris <102661546+GuillemBarris@users.noreply.github.com>
2024-12-17 16:09:19 +0100

    Upgrade using new part api Groups

--- Registres_curriculars_CRPGA/src/app/services/UserSchoolGroupService.ts
+++ Registres_curriculars_CRPGA/src/app/services/UserSchoolGroupService.ts
@@ -7,16 +7,18 @@ import { catchError, Observable, of } from 'rxjs';
 export class UserSchoolGroupService {
     private dbUrl = 'http://172.21.46.184:3000/api/v1/userSchoolGroup';
 
+    private dbUrl2 = 'http://172.21.46.184:3000/api/v1/courses';
+
     constructor(private http: HttpClient) {}
 
     getGroups(): Observable<any> {
-        return this.http.get<any>(`${this.dbUrl}/getGroup/`);
+        return this.http.get<any>(`${this.dbUrl2}/getGroup/?school=Escola Pia Olot`);
     }
     getSubjects(): Observable<any> {
         return this.http.get<any>(`${this.dbUrl}/getSubject/`);
     }
     getGrades(): Observable<any> {
-        return this.http.get<any>(`${this.dbUrl}/getGrade/`);
+        return this.http.get<any>(`${this.dbUrl2}/getGrade/?school=Escola Pia Olot`);
     }
     postUserSchoolGroup(user: any): Observable<any> {
         return this.http.post<any>(`${this.dbUrl}/createUserSchoolGroup/`, user).pipe(
