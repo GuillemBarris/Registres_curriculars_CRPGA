874c0c561b9eaa662a7f352f686134b54cf30c11
OctagonalDefence <eromero110@boscdelacoma.cat>
2024-12-18 17:19:18 +0100

    fetch the courses of a specific user from its email test fail

--- Registres_curriculars_CRPGA/src/app/test/Api/UserSchoolGroup/sda-service.service.spec.ts
+++ Registres_curriculars_CRPGA/src/app/test/Api/UserSchoolGroup/sda-service.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
+import { SdaServiceService } from '../../../services/sda-service.service';
 
-import { SdaServiceService } from './sda-service.service';
+
 
 describe('SdaServiceService', () => {
   let service: SdaServiceService;
@@ -13,4 +14,17 @@ describe('SdaServiceService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should fetch the courses of a specific user from its email' , () => {
+    const email = 'example@gmail.com';
+    const expectedCourses = ['course1', 'course2'];
+    
+    const httpSpy = spyOn(service.http, 'get').and.returnValue(of(expectedCourses));
+
+    service.getCourses(email).subscribe((courses) => {
+      expect(courses).toEqual(expectedCourses);
+      expect(httpSpy).toHaveBeenCalledWith(`${service.dbUrl}/courses/${email}`);
+    });
+  } );
+  
 });
